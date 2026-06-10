c5bc9da5e127a3271cae7b85109229038eeb85ed
Pranay Khojare <khojarepranay6@gmail.com>
2026-06-10 09:32:27 +0530

    Move MongoDB URL to environment variable

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-const mongoURL = 'mongodb+srv://user1:mane123@cluster1.w2ks0ib.mongodb.net/auth_task';
+// const mongoURL = 'mongodb+srv://user1:mane123@cluster1.w2ks0ib.mongodb.net/auth_task';
+const mongoURL = process.env.MONGO_URL;
 const connectionUrl = await mongoose.connect(mongoURL);
 console.log("Connected to MongoDB", connectionUrl.connection.host);
-
 
 const userSchema = new mongoose.Schema(
   {
@@ -27,9 +27,8 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
 const UserModel = mongoose.model("Users", userSchema);
-
 
 export default UserModel;
