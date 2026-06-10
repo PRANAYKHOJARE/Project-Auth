ca699b730ba0d81507e3a46e19df4ebeec0c7085
Pranay Khojare <khojarepranay6@gmail.com>
2026-06-10 09:52:55 +0530

    Add auth health check route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import { loginUser, registerUser } from '../controllers/auth.controller.js';
-import { authMiddleware } from '../middlewares/auth.middleware.js';
-import { googleLogin } from '../controllers/google.auth.controller.js';
-
+import express from "express";
+import { loginUser, registerUser } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { googleLogin } from "../controllers/google.auth.controller.js";
 
 const router = express.Router();
-router.post('/register', registerUser);
-router.post('/login', loginUser)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 // router.get("/profile", authMiddleware, (req, res) => {
 //     res.json(req.user);
@@ -15,5 +14,11 @@ router.post('/login', loginUser)
 //     })
 // });
 router.post("/google", googleLogin);
+
+router.get("/", (req, res) => {
+  res.json({
+    message: "Auth API is working",
+  });
+});
 
 export default router;
